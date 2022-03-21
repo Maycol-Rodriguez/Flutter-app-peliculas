68019b2414aefdd89f7b04785333a7b945c26c24
Maycol R <maycol.rodriguez.ma@gmail.com>
2022-03-21 01:06:27 -0500

    Agregando dependencias

---
+++ lib/ui/pantalla_detalles.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class PantallaDetalle extends StatelessWidget {
+
+  const PantallaDetalle({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Text('PantallaDetalle'),
+     ),
+   );
+  }
+}
---
+++ lib/ui/pantalla_principal.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class PantallaPrincipal extends StatelessWidget {
+
+  const PantallaPrincipal({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Text('PantallaPrincipal'),
+     ),
+   );
+  }
+}
